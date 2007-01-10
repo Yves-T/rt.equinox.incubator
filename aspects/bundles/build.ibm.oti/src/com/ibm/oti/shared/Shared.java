63f570c245357ea9f050599526e78240046eb3b0
mwebster <>
2007-01-10 17:45:10 +0000

    Fix JDK dependencies including IBM

---
+++ aspects/bundles/build.ibm.oti/src/com/ibm/oti/shared/Shared.java
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * Copyright (c) 2007 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *   Matthew Webster           initial implementation      
+ *******************************************************************************/
+
+package com.ibm.oti.shared;
+
+public class Shared {
+
+	public static boolean isSharingEnabled () {
+		throw new UnsupportedOperationException();
+	}
+	
+	public static SharedClassHelperFactory getSharedClassHelperFactory () {
+		throw new UnsupportedOperationException();
+	}
+}
