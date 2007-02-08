2b36ac46d717b7fb556da7d95cc23d69475568ed
Simon Kaegi <skaegi>
2007-02-08 23:11:36 +0000

    Moving some helper classes into http.helper. These are still being worked on and should in no way be considered API.

---
+++ server-side/bundles/org.eclipse.equinox.http.helper/src/org/eclipse/equinox/http/helper/BundleEntryHttpContext.java
@@ -0,0 +1,72 @@
+/*******************************************************************************
+ * Copyright (c) 2007 IBM Corporation, Cognos Incorporated and others
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.equinox.http.helper;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.framework.Bundle;
+import org.osgi.service.http.HttpContext;
+
+public class BundleEntryHttpContext implements HttpContext {
+	private Bundle bundle;
+	private String bundlePath;
+
+	public BundleEntryHttpContext(Bundle bundle) {
+		this.bundle = bundle;
+	}
+
+	public BundleEntryHttpContext(Bundle b, String bundlePath) {
+		this(b);
+		if (bundlePath != null) {
+			if (bundlePath.endsWith("/")) //$NON-NLS-1$
+				bundlePath = bundlePath.substring(0, bundlePath.length() - 1);
+
+			if (bundlePath.length() == 0)
+				bundlePath = null;
+		}
+		this.bundlePath = bundlePath;
+	}
+
+	public String getMimeType(String arg0) {
+		return null;
+	}
+
+	public boolean handleSecurity(HttpServletRequest arg0, HttpServletResponse arg1) throws IOException {
+		return true;
+	}
+
+	public URL getResource(String resourceName) {
+		if (bundlePath == null)
+			return bundle.getEntry(resourceName);
+		
+		return bundle.getEntry(bundlePath + resourceName);
+	}
+
+	public Set getResourcePaths(String path) {
+		Enumeration entryPaths = null;
+		if (bundlePath == null)
+			entryPaths = bundle.getEntryPaths(path);
+		else
+			entryPaths = bundle.getEntryPaths(bundlePath + path);
+		
+		if (entryPaths == null)
+			return null;
+
+		Set result = new HashSet();
+		while (entryPaths.hasMoreElements())
+			result.add(entryPaths.nextElement());
+		return result;
+	}
+}
---
+++ server-side/bundles/org.eclipse.equinox.http.helper/src/org/eclipse/equinox/http/helper/ContextListenerServletAdaptor.java
@@ -0,0 +1,39 @@
+package org.eclipse.equinox.http.helper;
+
+import java.io.IOException;
+import javax.servlet.*;
+
+public class ContextListenerServletAdaptor implements Servlet {
+	private ServletConfig config;
+	private ServletContextListener listener;
+	private Servlet delegate;
+
+	public ContextListenerServletAdaptor(ServletContextListener listener, Servlet delegate) {
+		this.listener = listener;
+		this.delegate = delegate;
+	}
+
+	public void init(ServletConfig config) throws ServletException {
+		this.config = config;
+		listener.contextInitialized(new ServletContextEvent(config.getServletContext()));
+		delegate.init(config);
+	}
+
+	public void service(ServletRequest req, ServletResponse resp) throws ServletException, IOException {
+		delegate.service(req, resp);
+	}
+
+	public void destroy() {
+		delegate.destroy();
+		listener.contextDestroyed(new ServletContextEvent(config.getServletContext()));
+		config = null;
+	}
+
+	public ServletConfig getServletConfig() {
+		return config;
+	}
+
+	public String getServletInfo() {
+		return "";
+	}
+}
