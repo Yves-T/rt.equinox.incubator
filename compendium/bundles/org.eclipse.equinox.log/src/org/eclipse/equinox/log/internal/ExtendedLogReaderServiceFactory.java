a0e9589cac40eaf5466d9d052449ce5220f04618
Simon Kaegi <skaegi>
2008-04-20 03:44:30 +0000

    Bug 227801 Add synchronization to extended log service

--- compendium/bundles/org.eclipse.equinox.log/src/org/eclipse/equinox/log/internal/ExtendedLogReaderServiceFactory.java
+++ compendium/bundles/org.eclipse.equinox.log/src/org/eclipse/equinox/log/internal/ExtendedLogReaderServiceFactory.java
@@ -1,12 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2006 Cognos Incorporated.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
+ * Copyright (c) 2006, 2008 Cognos Incorporated, IBM Corporation and others All
+ * rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *******************************************************************************/
+ ******************************************************************************/
 package org.eclipse.equinox.log.internal;
 
+import java.util.Collections;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -22,7 +23,17 @@ import org.osgi.service.log.LogListener;
 
 public class ExtendedLogReaderServiceFactory implements ServiceFactory {
 
-	private Map listeners = new HashMap();
+	private static final Enumeration EMPTY_ENUMERATION = new Enumeration() {
+		public boolean hasMoreElements() {
+			return false;
+		}
+
+		public Object nextElement() {
+			return null;
+		}
+	};
+
+	private Map listeners = Collections.synchronizedMap(new HashMap());
 
 	public Object getService(Bundle bundle, ServiceRegistration registration) {
 		return new LogReaderServiceImpl(this);
@@ -33,26 +44,30 @@ public class ExtendedLogReaderServiceFactory implements ServiceFactory {
 		readerService.shutdown();
 	}
 
-	public boolean isLoggable(Bundle bundle, String name, int level) {		
-		for (Iterator it = listeners.values().iterator(); it.hasNext();) {
-			LogFilter filter = (LogFilter) it.next();
-			if (filter.isLoggable(bundle, name, level))
-				return true;
-		}		
-		return false;		
+	public boolean isLoggable(Bundle bundle, String name, int level) {
+		synchronized (listeners) {
+			for (Iterator it = listeners.values().iterator(); it.hasNext();) {
+				LogFilter filter = (LogFilter) it.next();
+				if (filter.isLoggable(bundle, name, level))
+					return true;
+			}
+			return false;
+		}
 	}
 
 	public void log(Bundle bundle, String name, Object context, int level, String message, Throwable exception) {
 		LogEntry logEntry = new ExtendedLogEntryImpl(bundle, name, context, level, message, exception);
-		for (Iterator it = listeners.entrySet().iterator(); it.hasNext();) {
-			Entry entry = (Entry) it.next();
-			LogFilter filter = (LogFilter) entry.getValue();
-			if (filter.isLoggable(bundle, name, level)) {
-				LogListener listener = (LogListener) entry.getKey();
-				// should be sent asych
-				listener.logged(logEntry);
+		synchronized (listeners) {
+			for (Iterator it = listeners.entrySet().iterator(); it.hasNext();) {
+				Entry entry = (Entry) it.next();
+				LogFilter filter = (LogFilter) entry.getValue();
+				if (filter.isLoggable(bundle, name, level)) {
+					LogListener listener = (LogListener) entry.getKey();
+					// should be sent asych
+					listener.logged(logEntry);
+				}
 			}
-		}				
+		}
 	}
 
 	public void addLogListener(LogListener listener, LogFilter filter) {
@@ -64,14 +79,6 @@ public class ExtendedLogReaderServiceFactory implements ServiceFactory {
 	}
 
 	public Enumeration getLog() {
-		return new Enumeration() {
-			public boolean hasMoreElements() {
-				return false;
-			}
-
-			public Object nextElement() {
-				return null;
-			}
-		};
+		return EMPTY_ENUMERATION;
 	}
 }
