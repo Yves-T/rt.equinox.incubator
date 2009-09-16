a75e7f434f8feea93ceb6fb1a2edcee7bfcf0a26
Pascal Rapicault <prapicau>
2009-09-16 19:07:38 +0000

    *** empty log message ***

--- p2/bundles/org.eclipse.equinox.p2.diagnostic/src/org/eclipse/equinox/p2/diagnostic/RepositoryDumper.java
+++ p2/bundles/org.eclipse.equinox.p2.diagnostic/src/org/eclipse/equinox/p2/diagnostic/RepositoryDumper.java
@@ -1,6 +1,7 @@
 package org.eclipse.equinox.p2.diagnostic;
 
 import java.net.URI;
+import java.util.Properties;
 
 import org.eclipse.equinox.internal.provisional.p2.core.ProvisionException;
 import org.eclipse.equinox.internal.provisional.p2.metadata.IInstallableUnit;
@@ -17,7 +18,7 @@ public class RepositoryDumper {
 	}
 	
 	public void dump(URI targetRepository) throws ProvisionException {
-		IMetadataRepository repo = mgr.createRepository(targetRepository, "Dumped repository", null, null);
+		IMetadataRepository repo = mgr.createRepository(targetRepository, "Dumped repository", IMetadataRepositoryManager.TYPE_SIMPLE_REPOSITORY, new Properties());
 		mgr.removeRepository(targetRepository);
 		Collector c = mgr.query(InstallableUnitQuery.ANY, new Collector(), null);
 		repo.addInstallableUnits((IInstallableUnit[]) c.toArray(IInstallableUnit.class));
