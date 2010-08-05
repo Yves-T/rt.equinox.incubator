e1664da6b3c0efe05a52b903df17e0bdbaf81241
Thomas Watson <twatson>
2010-08-05 13:34:46 +0000

    Fix ambiguous call.

--- monitoring/bundles/org.eclipse.osgi.jmx/src/org/eclipse/osgi/jmx/internal/ServiceContributionProvider.java
+++ monitoring/bundles/org.eclipse.osgi.jmx/src/org/eclipse/osgi/jmx/internal/ServiceContributionProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006 IBM Corporation and others.
+ * Copyright (c) 2006, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -120,7 +120,7 @@ public class ServiceContributionProvider extends ContributionProvider {
 		}
 		BundleContext bc = Activator.getBundleContext();
 		try {
-			return bc.getServiceReferences(null, null);
+			return bc.getServiceReferences((String) null, (String) null);
 		} catch (InvalidSyntaxException e) {
 			// invalid syntax on null, null...
 		}
