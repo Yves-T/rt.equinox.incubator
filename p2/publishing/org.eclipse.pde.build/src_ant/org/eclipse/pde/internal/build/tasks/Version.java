6913c1964c8fe1578ec7e5847bb546e2c40dee1a
Pascal Rapicault <prapicau>
2011-02-13 15:19:41 +0000

    Merge HEAD

--- p2/publishing/org.eclipse.pde.build/src_ant/org/eclipse/pde/internal/build/tasks/Version.java
+++ p2/publishing/org.eclipse.pde.build/src_ant/org/eclipse/pde/internal/build/tasks/Version.java
@@ -1,5 +1,4 @@
 /*
- * $Header: /cvsroot/eclipse/pde/build/org.eclipse.pde.build/src_ant/org/eclipse/pde/internal/build/tasks/Version.java,v 1.2 2008/08/20 15:16:18 aniefer Exp $
  * 
  * Copyright (c) OSGi Alliance (2004, 2006). All Rights Reserved.
  * 
@@ -37,7 +36,7 @@ import java.util.StringTokenizer;
  * <p>
  * <code>Version</code> objects are immutable.
  * 
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.1 $
  * @since 1.3
  */
 
