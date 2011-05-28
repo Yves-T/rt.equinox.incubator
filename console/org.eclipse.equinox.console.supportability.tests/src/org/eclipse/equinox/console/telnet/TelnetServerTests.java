15102fe0bd2d9dab98fa6db1d1d285559f80cfc3
lkirchev <>
2011-05-28 12:38:36 +0000

    Add tests for the console bundles.

---
+++ console/org.eclipse.equinox.console.supportability.tests/src/org/eclipse/equinox/console/telnet/TelnetServerTests.java
@@ -0,0 +1,117 @@
+/*******************************************************************************
+ * Copyright (c) 2011 SAP AG
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Lazar Kirchev, SAP AG - initial contribution
+ ******************************************************************************/
+
+package org.eclipse.equinox.console.telnet;
+
+import org.apache.felix.service.command.CommandProcessor;
+import org.apache.felix.service.command.CommandSession;
+import org.easymock.EasyMock;
+import org.eclipse.equinox.console.common.ConsoleInputStream;
+import org.junit.Assert;
+import org.junit.Test;
+
+import org.osgi.framework.BundleContext;
+
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.ConnectException;
+import java.net.Socket;
+
+import static org.easymock.EasyMock.*;
+
+public class TelnetServerTests {
+
+    private static final String HOST = "localhost";
+    private static final int PORT = 38888;
+    private static final long WAIT_TIME = 5000;
+    private static final int TEST_CONTENT = 100;
+
+    @Test
+    public void testTelnetServer() throws Exception {
+
+    	CommandSession session = EasyMock.createMock(CommandSession.class);
+    	session.put((String)EasyMock.anyObject(), EasyMock.anyObject());
+        EasyMock.expectLastCall().times(3);
+        session.close();
+		EasyMock.expectLastCall();
+        EasyMock.replay(session);
+        
+        CommandProcessor processor = EasyMock.createMock(CommandProcessor.class);
+        EasyMock.expect(processor.createSession((ConsoleInputStream)EasyMock.anyObject(), (PrintStream)EasyMock.anyObject(), (PrintStream)EasyMock.anyObject())).andReturn(session);
+        EasyMock.replay(processor);
+        
+        TelnetServer telnetServer = new TelnetServer(null, processor, HOST, PORT);
+        telnetServer.start();
+        Socket socketClient = null;
+
+        try {
+            socketClient = new Socket("localhost", PORT);
+            OutputStream outClient = socketClient.getOutputStream();
+            outClient.write(TEST_CONTENT);
+            outClient.write('\n');
+            outClient.flush();
+            // wait for the accept thread to finish execution
+            try {
+                Thread.sleep(WAIT_TIME);
+            } catch (InterruptedException ie) {
+                // do nothing
+            }
+            return;
+        } catch(ConnectException e) {
+        	Assert.fail("Telnet port not open");
+        } finally {
+            if (socketClient != null) {
+                socketClient.close();
+            }
+            telnetServer.stopTelnetServer();
+        }
+    }
+
+    @Test
+    public void testTelnetServerWithoutHost() throws Exception {
+    	CommandSession session = EasyMock.createMock(CommandSession.class);
+    	session.put((String)EasyMock.anyObject(), EasyMock.anyObject());
+        EasyMock.expectLastCall().times(3);
+        EasyMock.replay(session);
+        
+        CommandProcessor processor = EasyMock.createMock(CommandProcessor.class);
+        EasyMock.expect(processor.createSession((ConsoleInputStream)EasyMock.anyObject(), (PrintStream)EasyMock.anyObject(), (PrintStream)EasyMock.anyObject())).andReturn(session);
+        EasyMock.replay(processor);
+        
+        TelnetServer telnetServer = new TelnetServer(null, processor, null, PORT);
+        telnetServer.start();
+        Socket socketClient = null;
+
+        try {
+            socketClient = new Socket("localhost", PORT);
+            OutputStream outClient = socketClient.getOutputStream();
+            outClient.write(TEST_CONTENT);
+            outClient.write('\n');
+            outClient.flush();
+
+            // wait for the accept thread to finish execution
+            try {
+                Thread.sleep(WAIT_TIME);
+            } catch (InterruptedException ie) {
+                // do nothing
+            }
+        } catch(ConnectException e) {
+        	Assert.fail("Telnet port not open");
+        } finally {
+            if (socketClient != null) {
+                socketClient.close();
+            }
+            telnetServer.stopTelnetServer();
+        }
+
+    }
+
+}
